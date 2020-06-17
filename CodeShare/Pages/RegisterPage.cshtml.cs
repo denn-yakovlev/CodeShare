1be900ea616a6d7927e134e036cda8b240bd4523
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-06-17 22:12:19 +0300

    Registration added

--- CodeShare/Pages/RegisterPage.cshtml.cs
+++ CodeShare/Pages/RegisterPage.cshtml.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Security.Claims;
-using System.Security.Policy;
 using System.Threading.Tasks;
 using CodeShare.Services.DatabaseInteractor;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CodeShare.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 
 namespace CodeShare.Pages
 {
-    public class LoginModel : PageModel
+    public class RegisterPageModel : PageModel
     {
+
         [BindProperty]
         [Required]
         [MinLength(3, ErrorMessage = "Password should have at least 3 characters")]
@@ -24,33 +25,46 @@ namespace CodeShare.Pages
         [MinLength(8, ErrorMessage = "Password should have at least 8 characters")]
         public string Password { get; set; }
 
+        [BindProperty]
+        [Required]
+        [MinLength(8, ErrorMessage = "Password should have at least 8 characters")]
+        public string RepeatedPassword { get; set; }
+
         public IDatabaseInteractor DbInteractor { get; }
 
-        public LoginModel(IDatabaseInteractor dbInteractor)
+        public RegisterPageModel(IDatabaseInteractor dbInteractor)
         {
             DbInteractor = dbInteractor;
         }
 
         public void OnGet()
         {
-            
+
         }
 
         public async Task<IActionResult> OnPostAsync(string returnRoute)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && Password == RepeatedPassword)
             {
-                var claims = new List<Claim>
+                var user = (await DbInteractor.Users.FilterAsync(user => user.UserName == UserName)).SingleOrDefault();
+                Task registrationTask;
+                if (user == null)
                 {
-                    new Claim(ClaimTypes.Name, UserName),
-                    new Claim("Password", Password)
-                };
-                var identity = new ClaimsIdentity(claims, "Cookies");
-                var user = DbInteractor.Users.Filter(user => user.UserName == UserName).SingleOrDefault();
-                if (user != null)
+                    var newUser = new User { UserName = UserName, Password = Password };
+                    registrationTask = DbInteractor.Users.CreateAsync(newUser);
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, UserName),
+                        new Claim("Password", Password)
+                    };
+                    var identity = new ClaimsIdentity(claims, "Cookies");
                     await HttpContext.SignInAsync("Cookies", new ClaimsPrincipal(identity));
+                    await registrationTask;
+                }
                 else
-                    return Redirect("/error");
+                {
+                    return Redirect("/Login");
+                }
                 if (Url.IsLocalUrl(returnRoute))
                     return Redirect(returnRoute);
                 return RedirectToPage("_Host");
